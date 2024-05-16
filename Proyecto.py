36c220bdba80197e15d13b14abca55f9662da8ec
111Spark111 <carlosriveraquezada@gmail.com>
2024-05-16 00:38:16 -0400

    tersera actualizacion (inicio)

--- Proyecto.py
+++ Proyecto.py
@@ -22,50 +22,13 @@ def pantalla():
     fondos=OptionMenu(ventana,op,*elecciones,command= lambda fon:jugador.Fondo(fon,primer_fondo))
     fondos.place(x=895,y=8)
 
-    #-------------------------------------------------------------------------------Trasfondo
+    #Trasfondo
     iniciacion0_0=Label(ventana,text="- En tierras lejanas donde los héroes y monstruos abundan, un grupo de amigos se reúne\ncon el deseo de ser aventureros. Explorando los bosques y derrotando monstruos.       ",bg="black",fg="white")
     iniciacion0_0.place(x=255,y=360)
     cont1=Button(ventana,text="Continuar >>>",bg="orange",fg="white",command= lambda: jugador.tr(iniciacion0_0,cont1))
     cont1.place(x=650,y=450)
 
-
-         #  <------------------------------------------------------------------------------------------------X
-
-    # Instrucciones iniciales
-    iniciacion1_0=Label(ventana,text="- Elija a su personaje aventurero (No podras cambiarlo despues).",bg="black",fg="white")
-    iniciacion1_0.place(x=255,y=70)
-    iniciacion1_1=Label(ventana,text="- Indique su nombre aventurero (No podras cambiarlo despues).",bg="black",fg="white")
-    iniciacion1_1.place(x=255,y=360) 
-
-    # Ingreso del nombre:
-    nombre=Entry(ventana) 
-    nombre.place(x=410,y=450)
-    nom=Button(ventana,text="aceptar",bg="orange",fg="white",command=lambda:jugador.Nombre(nombre,iniciacion1_1,nombre,nom))
-    nom.place(x=520,y=448)
-
-    # eleccion de personaje:
-    des_v=" Vida:  90                                          Vida:  120                                       Vida:  100"
-    des_f="  Fuerza:  15                                      Fuerza:  50                                     Fuerza:  25"
-    des_m="Mana:  25                                        Mana:  5                                        Mana:  10"
-    descripcion=Label(ventana,text=f"{des_v}\n{des_f}\n{des_m}",bg="black",fg="white")
-    descripcion.place(x=255,y=235)
-    mago=Button(ventana,text= "Mago",bg="orange",fg="white",command= lambda: jugador.Personaje(iniciacion1_0,descripcion,mago,caballero,arquero,magico,fuerte,velos,1))
-    mago.place(x=315,y=300)
-    ima_mago = PhotoImage(file=f"C:\\Users\\carlo\\OneDrive\\Escritorio\\aventura\\personaje\\Maga1.png") 
-    magico = Label(ventana, image=ima_mago)
-    magico.place(x=270,y=100)
-    caballero=Button(ventana,text= "Caballero",bg="orange",fg="white",command= lambda: jugador.Personaje(iniciacion1_0,descripcion,mago,caballero,arquero,magico,fuerte,velos,2))
-    caballero.place(x=470,y=300)
-    ima_caba = PhotoImage(file=f"C:\\Users\\carlo\\OneDrive\\Escritorio\\aventura\\personaje\\caballero1.png") 
-    fuerte = Label(ventana, image=ima_caba)
-    fuerte.place(x=435,y=100)
-    arquero=Button(ventana,text= "Arquero",bg="orange",fg="white",command= lambda: jugador.Personaje(iniciacion1_0,descripcion,mago,caballero,arquero,magico,fuerte,velos,3))
-    arquero.place(x=635,y=300)
-    ima_arqu = PhotoImage(file=f"C:\\Users\\carlo\\OneDrive\\Escritorio\\aventura\\personaje\\Arquero1.png") 
-    velos = Label(ventana, image=ima_arqu)
-    velos.place(x=600,y=100)
-
-    #ima_arqu = PhotoImage(file=f"C:\\Users\\carlo\\OneDrive\\Escritorio\\aventura\\personaje\\bosque.png") 
+    #ima_arqu = PhotoImage(file="C:\\Users\\carlo\\OneDrive\\Escritorio\\aventura\\personaje\\bosque.png") 
     #velos = Label(ventana, image=ima_arqu,bg="black")
     #velos.place(x=249,y=64)
 
--- librerias.py
+++ librerias.py
@@ -9,6 +9,7 @@ class Eleccion:
     self.primer_fondo = Label(ventana, image=self.imagen_fondo)
     self.primer_fondo.place(x=0, y=0)
     self.f=""
+    self.cont2=None
 
   def tr(self,a,s):
     self.destructor(a,s)
@@ -16,12 +17,56 @@ class Eleccion:
     ww=" ogro. El cual secuestraria a dos integrantes del equipo…¡Menos a ti aventurero!                    "
     iniciacion0_1=Label(self.ventana,text=f"{qq}\n{ww}",bg="black",fg="white")
     iniciacion0_1.place(x=255,y=360)
-    cont2=Button(self.ventana,text="Continuar >>>",bg="orange",fg="white",command= lambda: self.destructor(iniciacion0_1,cont2))
-    cont2.place(x=650,y=450)
+
+    self.cont2=Button(self.ventana,text="Continuar >>>",bg="orange",fg="white",command= lambda: self.nom_per(iniciacion0_1,self.cont2))
+    self.cont2.place(x=650,y=450)
+
+
+  def nom_per(self,*x):
+    for e in x:
+      e.destroy()
+
+    if not self.cont2.winfo_exists():
+        # Instrucciones iniciales
+        iniciacion1_0=Label(self.ventana,text="- Elija a su personaje aventurero (No podras cambiarlo despues).",bg="black",fg="white")
+        iniciacion1_0.place(x=255,y=70)
+        iniciacion1_1=Label(self.ventana,text="- Indique su nombre aventurero (No podras cambiarlo despues).",bg="black",fg="white")
+        iniciacion1_1.place(x=255,y=360) 
+
+        # Ingreso del nombre:
+        nombre=Entry(self.ventana) 
+        nombre.place(x=410,y=450)
+        nom=Button(self.ventana,text="aceptar",bg="orange",fg="white",command=lambda: self.Nombre(nombre,iniciacion1_1,nombre,nom))
+        nom.place(x=520,y=448)
+
+        # eleccion de personaje:
+        des_v=" Vida:  90                                          Vida:  120                                       Vida:  100"
+        des_f="  Fuerza:  15                                      Fuerza:  50                                     Fuerza:  25"
+        des_m="Mana:  25                                        Mana:  5                                        Mana:  10"
+        descripcion=Label(self.ventana,text=f"{des_v}\n{des_f}\n{des_m}",bg="black",fg="white")
+        descripcion.place(x=255,y=235)
+        mago=Button(self.ventana,text= "Mago",bg="orange",fg="white",command= lambda: self.Personaje(iniciacion1_0,descripcion,mago,caballero,arquero,magico,fuerte,velos,1))
+        mago.place(x=315,y=300)
+        self.ima_mago = PhotoImage(file="C:\\Users\\carlo\\OneDrive\\Escritorio\\aventura\\personaje\\Maga1.png") 
+        magico = Label(self.ventana, image=self.ima_mago)
+        magico.place(x=270,y=100)
+        caballero=Button(self.ventana,text= "Caballero",bg="orange",fg="white",command= lambda: self.Personaje(iniciacion1_0,descripcion,mago,caballero,arquero,magico,fuerte,velos,2))
+        caballero.place(x=470,y=300)
+        self.ima_caba = PhotoImage(file="C:\\Users\\carlo\\OneDrive\\Escritorio\\aventura\\personaje\\caballero1.png") 
+        fuerte = Label(self.ventana, image=self.ima_caba)
+        fuerte.place(x=435,y=100)
+        arquero=Button(self.ventana,text= "Arquero",bg="orange",fg="white",command= lambda: self.Personaje(iniciacion1_0,descripcion,mago,caballero,arquero,magico,fuerte,velos,3))
+        arquero.place(x=635,y=300)
+        self.ima_arqu = PhotoImage(file="C:\\Users\\carlo\\OneDrive\\Escritorio\\aventura\\personaje\\Arquero1.png") 
+        velos = Label(self.ventana, image=self.ima_arqu)
+        velos.place(x=600,y=100)
+
+
+    
 
   def destructor(self,*x): # Destrulle todo lo que le entra
-     for e in x:
-         e.destroy()     
+    for e in x:
+      e.destroy()        
 
   def Fondo(self,fon,primer_fondo): # Cambia el fondo
     self.destructor(primer_fondo)
@@ -81,6 +126,4 @@ class Jugador:
     self.vida=vida
     self.fuerza=fuerza
     self.mana=mana
-  
-
     
